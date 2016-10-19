b315f192fac2ee0865525adb9381ba0cbc91af4f
Thomas McClish <mcclth@umich.edu>
2016-10-18 22:49:34 -0400

    Initial Commit

---
+++ VInputContainer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VInputContainer
+//
+//  Created by McClish, Thomas on 10/18/16.
+//  Copyright © 2016 EECS481. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VInputContainer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  VInputContainer
+//
+//  Created by McClish, Thomas on 10/18/16.
+//  Copyright © 2016 EECS481. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
