bad9bb4940f9236804735a39f2f0a0031ad491b2
Thomas McClish <mcclth@umich.edu>
2016-10-18 23:48:37 -0400

    Initial project, fullscreen keyboard extension

---
+++ VInput/KeyboardViewController.h
@@ -0,0 +1,15 @@
+//
+//  KeyboardViewController.h
+//  VInput
+//
+//  Created by McClish, Thomas on 10/18/16.
+//  Copyright © 2016 EECS481. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@property (nonatomic) NSLayoutConstraint *heightConstraint;
+
+@end
