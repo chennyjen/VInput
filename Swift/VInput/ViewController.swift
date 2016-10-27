27de4301f33ed2b5b66e4a9eb377c99cd6f2ac53
McClish <mcclth@aha16.ccs.itd.umich.edu>
2016-10-27 17:42:20 -0400

    Added Swift version

---
+++ Swift/VInput/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VInput
+//
+//  Created by McClish, Thomas on 10/27/16.
+//  Copyright © 2016 EECS481-VInput. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
