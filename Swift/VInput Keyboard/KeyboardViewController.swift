bf88a3c7fdb886f37f7272aec0b1ebf2831fb39b
Thomas McClish <mcclth@umich.edu>
2016-10-27 21:57:46 -0400

    Added gesture recognizers. Fixed fullscreen view layout on load. Added adjust on device rotation.

--- Swift/VInput Keyboard/KeyboardViewController.swift
+++ Swift/VInput Keyboard/KeyboardViewController.swift
@@ -11,64 +11,111 @@ import UIKit
 class KeyboardViewController: UIInputViewController {
 
     @IBOutlet var nextKeyboardButton: UIButton!
-    
-    let portraitHeight:CGFloat = 256.0
-    let landscapeHeight:CGFloat = 203.0
-    
-    let tapRec = UITapGestureRecognizer()
-    
+    var fullView: UIView!
+    let singleTapRecognizer = UITapGestureRecognizer()
+    let doubleTapRecognizer = UITapGestureRecognizer()
+    let swipeLeftRecognizer = UISwipeGestureRecognizer()
+    let swipeDownRecognizer = UISwipeGestureRecognizer()
+    let swipeRightRecognizer = UISwipeGestureRecognizer()
+    let swipeUpRecognizer = UISwipeGestureRecognizer()
     var heightConstraint: NSLayoutConstraint?
     
     override func updateViewConstraints() {
         super.updateViewConstraints()
         
         // Add custom view sizing constraints here
-        /*if (self.view.frame.size.width == 0 || self.view.frame.size.height == 0) {
+        if (self.view.frame.size.width == 0 || self.view.frame.size.height == 0) {
             return
         }
         
-        inputView!.removeConstraint(heightConstraint!)
+        view.removeConstraint(heightConstraint!)
+        
         heightConstraint!.constant = UIScreen.main.bounds.size.height * 0.9
-        inputView?.addConstraint(heightConstraint!)*/
+        view.addConstraint(heightConstraint!)
+        print("--- Set main view height to", heightConstraint?.constant)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Perform custom UI setup here
-        self.nextKeyboardButton = UIButton(type: .system)
+        // Set up UIView over full area to accept gestures
+        fullView = UIView()
+        fullView.backgroundColor = UIColor.gray
+        fullView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(fullView)
+        fullView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        fullView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        fullView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        fullView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
-        self.nextKeyboardButton.sizeToFit()
-        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        // Set gesture recognizer targets and values
+        singleTapRecognizer.numberOfTapsRequired = 1
+        singleTapRecognizer.addTarget(self, action: #selector(onSingleTap))
+        singleTapRecognizer.require(toFail: doubleTapRecognizer)
         
-        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        doubleTapRecognizer.addTarget(self, action: #selector(onDoubleTap))
         
-        self.view.addSubview(self.nextKeyboardButton)
+        swipeLeftRecognizer.direction = UISwipeGestureRecognizerDirection.left
+        swipeLeftRecognizer.addTarget(self, action: #selector(onSwipeLeft))
         
-        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        swipeDownRecognizer.direction = UISwipeGestureRecognizerDirection.down
+        swipeDownRecognizer.addTarget(self, action: #selector(onSwipeDown))
         
-        /*
-        heightConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1.0, constant: portraitHeight)
+        swipeRightRecognizer.direction = UISwipeGestureRecognizerDirection.right
+        swipeRightRecognizer.addTarget(self, action: #selector(onSwipeRight))
+        
+        swipeUpRecognizer.direction = UISwipeGestureRecognizerDirection.up
+        swipeUpRecognizer.addTarget(self, action: #selector(onSwipeUp))
+        
+        // Add gesture recognizers to fullView
+        fullView.addGestureRecognizer(doubleTapRecognizer)
+        fullView.addGestureRecognizer(singleTapRecognizer)
+        fullView.addGestureRecognizer(swipeLeftRecognizer)
+        fullView.addGestureRecognizer(swipeDownRecognizer)
+        fullView.addGestureRecognizer(swipeRightRecognizer)
+        fullView.addGestureRecognizer(swipeUpRecognizer)
+        
+        // Default Next Keyboard button
+         nextKeyboardButton = UIButton(type: .system)
+         nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
+         nextKeyboardButton.sizeToFit()
+         nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+         nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+         fullView.addSubview(nextKeyboardButton)
+         nextKeyboardButton.leftAnchor.constraint(equalTo: fullView.leftAnchor).isActive = true
+         nextKeyboardButton.bottomAnchor.constraint(equalTo: fullView.bottomAnchor).isActive = true
+        
+        // TODO fix this to check for orientation and set constraint to desired value
+        heightConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1.0, constant: 256)
         
         heightConstraint!.priority = 999.0
-        */
-        
-        // -------------------- gesture testing
-        tapRec.addTarget(self, action: #selector(tappedView))
-        view.addGestureRecognizer(tapRec)
-        view.isUserInteractionEnabled = true
-        
     }
     
-    func tappedView(){
-        print("481 sucks")
+    func onSingleTap(){
+        print("--- Single Tapped")
     }
     
-//    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-//        return true
-//    }
+    func onDoubleTap(){
+        print("--- Double Tapped")
+    }
+    
+    func onSwipeLeft(){
+        print("--- Swipe Left")
+    }
+    
+    func onSwipeDown(){
+        print("--- Swipe Down")
+    }
+    
+    func onSwipeRight(){
+        print("--- Swipe Right")
+    }
+    
+    func onSwipeUp(){
+        print("--- Swipe Up")
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -91,5 +138,9 @@ class KeyboardViewController: UIInputViewController {
         }
         self.nextKeyboardButton.setTitleColor(textColor, for: [])
     }
-
+    
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        print("--- Rotated")
+        updateViewConstraints()
+    }
 }
