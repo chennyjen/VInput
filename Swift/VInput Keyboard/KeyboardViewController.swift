d85c2320214b13bb1061977fee72edb5ebaf76d2
Michael Vander Lugt <mvlugt@umich.edu>
2016-11-06 16:08:11 -0500

    ryan + thomas merge; insert space, read back character

--- Swift/VInput Keyboard/KeyboardViewController.swift
+++ Swift/VInput Keyboard/KeyboardViewController.swift
@@ -23,7 +23,8 @@ class KeyboardViewController: UIInputViewController {
     let swipeUpRecognizer = UISwipeGestureRecognizer()
     //let panFromTopRecognizer = UIScreenEdgePanGestureRecognizer() - have to suppress opening notification center for this to work
     let pinchRecognizer = UIPinchGestureRecognizer()
-    let holdRecognizer = UILongPressGestureRecognizer()
+    let shortHoldRecognizer = UILongPressGestureRecognizer()
+    let longHoldRecognizer = UILongPressGestureRecognizer()
     var heightConstraint: NSLayoutConstraint?
     let alphabet: [String] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
                               "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -33,6 +34,8 @@ class KeyboardViewController: UIInputViewController {
     var insertedPeriod: Bool = false
     var utterance: AVSpeechUtterance!
     let speechSynthesizer = AVSpeechSynthesizer()
+    var searching: Bool = false
+    var newWord: Bool = true
     
     override func updateViewConstraints() {
         super.updateViewConstraints()
@@ -51,7 +54,6 @@ class KeyboardViewController: UIInputViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Perform custom UI setup here
         // Set up UIView over full area to accept gestures
         fullView = UIView()
@@ -82,12 +84,14 @@ class KeyboardViewController: UIInputViewController {
         letterLabel.centerXAnchor.constraint(equalTo: fullView.centerXAnchor).isActive = true
         letterLabel.centerYAnchor.constraint(equalTo: fullView.centerYAnchor).isActive = true
         letterLabel.textColor = UIColor.black
-        rewrite()
+        
+        
+        announceLetter()
         
         // Set gesture recognizer targets and values
-        singleTapRecognizer.numberOfTapsRequired = 1
-        singleTapRecognizer.addTarget(self, action: #selector(onSingleTap))
-        singleTapRecognizer.require(toFail: doubleTapRecognizer)
+//        singleTapRecognizer.numberOfTapsRequired = 1
+//        singleTapRecognizer.addTarget(self, action: #selector(onSingleTap))
+//        singleTapRecognizer.require(toFail: doubleTapRecognizer)
         
         doubleTapRecognizer.numberOfTapsRequired = 2
         doubleTapRecognizer.numberOfTouchesRequired = 1
@@ -115,9 +119,9 @@ class KeyboardViewController: UIInputViewController {
         
         pinchRecognizer.addTarget(self, action: #selector(onPinch))
         
-        holdRecognizer.minimumPressDuration = TimeInterval(1)
-        holdRecognizer.allowableMovement = 50
-        holdRecognizer.addTarget(self, action: #selector(onHold))
+        shortHoldRecognizer.minimumPressDuration = TimeInterval(1)
+        shortHoldRecognizer.allowableMovement = 50
+        shortHoldRecognizer.addTarget(self, action: #selector(onHold))
         
         // Add gesture recognizers to fullView
         fullView.addGestureRecognizer(doubleTapRecognizer)
@@ -129,7 +133,7 @@ class KeyboardViewController: UIInputViewController {
         fullView.addGestureRecognizer(swipeUpRecognizer)
         //fullView.addGestureRecognizer(panFromTopRecognizer)
         fullView.addGestureRecognizer(pinchRecognizer)
-        fullView.addGestureRecognizer(holdRecognizer)
+        fullView.addGestureRecognizer(shortHoldRecognizer)
         
         // TODO fix this to check for orientation and set constraint to desired value
         heightConstraint = NSLayoutConstraint(item: view, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1.0, constant: 256)
@@ -137,44 +141,57 @@ class KeyboardViewController: UIInputViewController {
         heightConstraint!.priority = 999.0
     }
     
-    func onSingleTap() {
-        print("--- Single Tapped")
-    }
-    
     func onDoubleTap() {
-        speak(textToSpeak: word)
+//        self.textDocumentProxy.documentContextBeforeInput!
+        let mid = Int(ceil(Double(rIndex - lIndex)/2)) + lIndex - 1
+        let text = "Left or right of " + alphabet[mid]
+        speak(textToSpeak: text)
     }
     
+//    Holder for reading back word
     func onDoubleTapTwoTouch() {
         for character in word.characters {
             speak(textToSpeak: String(character))
         }
     }
-    
-    // navigate left
+
     func onSwipeLeft() {
-        rIndex = Int(ceil(Double(rIndex - lIndex)/2)) + lIndex - 1
-        checkDone()
-    }
-    
-    func onSwipeDown() {
-        (textDocumentProxy as UIKeyInput).deleteBackward()
-        speak(textToSpeak: "backspace")
-        if word.characters.count > 0 {
-            word.remove(at: word.index(before: word.endIndex))
+        searching = true
+        if !isDone() {
+            rIndex = Int(ceil(Double(rIndex - lIndex)/2)) + lIndex - 1
+            announceLetter()
         }
     }
     
-    // navigate right
+    func onSwipeDown() {
+        if searching {
+            restartSearch()
+        }
+        else {
+            speak(textToSpeak: "Deleting previous character", postDelay: TimeInterval(4))
+            self.textDocumentProxy.deleteBackward()
+            announceLetter()
+        }
+    }
+    
     func onSwipeRight() {
-        lIndex += Int(ceil(Double(rIndex - lIndex)/2.0))
-        checkDone()
+        searching = true
+        if !isDone() {
+            lIndex += Int(ceil(Double(rIndex - lIndex)/2.0))
+            announceLetter()
+        }
     }
     
     func onSwipeUp() {
-        lIndex = Int(ceil(Double(rIndex - lIndex)/2)) + lIndex - 1
-        rIndex = lIndex
-        checkDone()
+        let mid = Int(ceil(Double(rIndex - lIndex)/2)) + lIndex - 1
+        self.textDocumentProxy.insertText(alphabet[mid])
+        
+        word = newWord ? alphabet[mid] : word + alphabet[mid]
+        newWord = false
+        
+        let text = alphabet[mid] + " inserted"
+        speak(textToSpeak: text, pitchMultiplier: 1.0, postDelay: TimeInterval(4))
+        restartSearch()
     }
     
 //    func onPanFromTop() {
@@ -188,42 +205,49 @@ class KeyboardViewController: UIInputViewController {
     }
     
     func onHold() {
-        if holdRecognizer.state == UIGestureRecognizerState.began {
+        // TODO: CLEAN UP
+        if shortHoldRecognizer.state == UIGestureRecognizerState.began {
+            self.textDocumentProxy.insertText(" ")
+            newWord = true
+            speak(textToSpeak: "Space inserted")
+            
             // TODO when Settings bundle is implemented, this behavior should be a setting
-            if word == "" && !insertedPeriod {
-                (textDocumentProxy as UIKeyInput).deleteBackward()
-                (textDocumentProxy as UIKeyInput).insertText(". ")
-                speak(textToSpeak: "period inserted")
-                insertedPeriod = true
-            }
-            else {
-                (textDocumentProxy as UIKeyInput).insertText(" ")
-                speak(textToSpeak: "space")
-                word = ""
-            }
+//            if word == "" && !insertedPeriod {
+//                (textDocumentProxy as UIKeyInput).deleteBackward()
+//                (textDocumentProxy as UIKeyInput).insertText(". ")
+//                speak(textToSpeak: "period inserted")
+//                insertedPeriod = true
+//            }
+//            else {
+//            }
         }
     }
     
-    func checkDone() {
+    func isDone() -> Bool {
         if(lIndex == rIndex)
         {
-            (textDocumentProxy as UIKeyInput).insertText(alphabet[lIndex])
-            word += alphabet[lIndex]
             // speak with a lower pitch when announcing a finalized letter,
             // and put a one second delay after speech so call to rewrite 
             // doesn't immediately speak the next option
-            speak(textToSpeak: alphabet[lIndex], pitchMultiplier: 0.75, postDelay: TimeInterval(0.5))
-            lIndex = 0
-            rIndex = 25
-            insertedPeriod = false
+            let text = "Swipe up to select " + String(alphabet[lIndex]) + ", swipe down to restart search"
+            speak(textToSpeak: text, pitchMultiplier: 0.75, postDelay: TimeInterval(0.5))
         }
-        rewrite()
+        return lIndex == rIndex
     }
     
-    func rewrite() {
+    func restartSearch() {
+        lIndex = 0
+        rIndex = 25
+        searching = false
+        insertedPeriod = false
+        announceLetter()
+    }
+    
+    func announceLetter() {
         let nextMid = Int(ceil(Double(rIndex - lIndex)/2)) + lIndex - 1
-        letterLabel.text = alphabet[nextMid]
-        speak(textToSpeak: alphabet[nextMid])
+        letterLabel.text = String(alphabet[nextMid])
+        let text = "Left or right of " + alphabet[nextMid]
+        speak(textToSpeak: text)
     }
     
     func speak(textToSpeak: String, pitchMultiplier: Float = 1.0, postDelay: TimeInterval = TimeInterval(0)) {
@@ -231,7 +255,7 @@ class KeyboardViewController: UIInputViewController {
         // TODO some of these values should be exposed as options in the Settings bundle
         utterance.pitchMultiplier = pitchMultiplier
         //utterance.postUtteranceDelay = postDelay
-        utterance.rate = 0.6
+        utterance.rate = 0.5
         speechSynthesizer.speak(utterance)
     }
     
