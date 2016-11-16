1cf68e0130fe4418009f4701afa33f77469af167
System Administrator <mvlugt@umich.edu>
2016-11-16 13:33:02 -0500

    refactor: added Mode protocol w/ training subclass

---
+++ Swift/VInput Keyboard/TutorialMode.swift
@@ -0,0 +1,21 @@
+//
+//  TutorialMode.swift
+//  VInput
+//
+//  Created by Michael Vander Lugt on 11/16/16.
+//  Copyright © 2016 EECS481-VInput. All rights reserved.
+//
+
+import Foundation
+
+
+class TutorialMode : InputMode {
+    
+    override func onSwipeLeft() {
+        return
+    }
+    
+    override func onSwipeRight() {
+        return
+    }
+}
