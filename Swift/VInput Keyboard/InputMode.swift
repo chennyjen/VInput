f701c166314e6d3d16e32e296b1f772b6a05e32a
Ryan Cesiel <ryan.cesiel@gmail.com>
2016-11-19 20:17:06 -0500

    logic for counting a words as they are inserted by user

--- Swift/VInput Keyboard/InputMode.swift
+++ Swift/VInput Keyboard/InputMode.swift
@@ -9,6 +9,7 @@
 import Foundation
 import AVFoundation
 import UIKit
+import CoreData
 
 class InputMode : Mode {
     
@@ -90,13 +91,74 @@ class InputMode : Mode {
         SpeechUtil.speak(textToSpeak: "Inserting space")
         keyboardController.textDocumentProxy.insertText(" ")
         currentWord = ""
+        
+        // Save word to Core Data
+        let lastWordTyped = loadFromProxy()
+        
+        let context = self.keyboardController.persistentContainer!.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>()
+        request.predicate = NSPredicate(format: "word = %@", lastWordTyped)
+        request.entity = NSEntityDescription.entity(forEntityName: "TypedWord", in: context)
+        
+        do {
+            let results = try context.fetch(request)
+            let wordToInsertOrUpdate: TypedWord?
+            if results.count == 0 {
+                wordToInsertOrUpdate = NSEntityDescription.insertNewObject(forEntityName: "TypedWord", into: context) as! TypedWord
+                wordToInsertOrUpdate!.word = lastWordTyped
+                wordToInsertOrUpdate!.frequency = 0
+            } else {
+                wordToInsertOrUpdate = (results[0] as! TypedWord)
+            }
+            wordToInsertOrUpdate!.frequency += 1
+            try context.save()
+        } catch {
+            let fetchError = error as NSError
+            print(fetchError)
+        }
+        
+        // PRINT ALL WORDS
+        let request2 = NSFetchRequest<NSFetchRequestResult>()
+        request2.entity = NSEntityDescription.entity(forEntityName: "TypedWord", in: context)
+        
+        do {
+            let results = try context.fetch(request2)
+            for result in results {
+                print((result as! TypedWord).word)
+                print((result as! TypedWord).frequency)
+            }
+        } catch {
+            let fetchError = error as NSError
+            print(fetchError)
+        }
+        
+        //        SEARCHING FOR WORD
+        //        let request = NSFetchRequest<NSFetchRequestResult>()
+        //        request.predicate = NSPredicate(format: "word beginswith[c] %@", word)
+        //        request.entity = NSEntityDescription.entity(forEntityName: "TypedWord", in: context)
+        //
+        //        do {
+        //            let results = try context.fetch(request)
+        //            if results.count > 0 {
+        //                // suggestion = results[0]
+        //            }
+        //
+        //        } catch {
+        //            let fetchError = error as NSError
+        //            print(fetchError)
+        //        }
+        // if word in DB, increment
+        // else
+        // add to DB and increment
+        
+        //Not sure if this should be here
         values.isSearchingThenReset()
         VisualUtil.updateViewAndAnnounce(letter: values.getCurrentValue())
+
     }
     
     private func loadFromProxy() -> String {
         let textInDocumentProxy : [String] = keyboardController.textDocumentProxy.documentContextBeforeInput!.components(separatedBy: " ").filter{$0.isEmpty == false}
         return textInDocumentProxy.isEmpty ? "" : textInDocumentProxy.last!
-    
     }
 }
